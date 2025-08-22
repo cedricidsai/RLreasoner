1fd19903c79d78770ce2a33e3ce25618bfdf2e84
Cedric Mesnage <cedricmesnage@MD4DXGQ2P9.local>
2025-08-22 11:19:44 +0200

    init

--- rlreasoner_config.py
+++ rlreasoner_config.py
@@ -21,7 +21,7 @@ models = [
         path='meta-llama/Llama-2-7b-hf',
         # path="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
         # model=base_model,
-        # model_kwargs={'token':os.environ['TOKEN']},
+        model_kwargs={'token':os.environ['HF_TOKEN']},
         max_out_len=512,
     )
 ]
