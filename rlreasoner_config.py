a6c6366c543fefe9ba566af9c304bb6418c92901
Cedric Mesnage <cedricmesnage@MD4DXGQ2P9.local>
2025-08-22 19:28:46 +0200

    init

--- rlreasoner_config.py
+++ rlreasoner_config.py
@@ -1,31 +1,31 @@
 # rlreasoner_config.py
-from opencompass.models.rlreasoner import RLReasoner
+# from opencompass.models.rlreasoner import RLReasoner
 from opencompass.models import HuggingFaceCausalLM
 import os
 
 
 # 'meta-llama/Llama-2-7b-hf'
 # Define the underlying base model (example: LLaMA-7B from HF)
-base_model = dict(
-    type=HuggingFaceCausalLM,
-    # path="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
-    # path='meta-llama/Llama-2-7b-hf',
-    path = 'Qwen/Qwen2.5-0.5B-Instruct',
-    max_out_len=512,
-)
+# base_model = dict(
+#     type=HuggingFaceCausalLM,
+#     # path="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
+#     # path='meta-llama/Llama-2-7b-hf',
+#     path = 'Qwen/Qwen2.5-0.5B-Instruct',
+#     max_out_len=512,
+# )
 
 # Wrap our AGI around the base model
 models = [
     dict(
         abbr='rlreasoner',
-        type='HuggingFaceCausalLM',
+        type=HuggingFaceCausalLM,
         path = 'Qwen/Qwen2.5-0.5B-Instruct',
-        # path='meta-llama/Llama-2-7b-hf',
-        # path="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
-        # model=base_model,
-        model_kwargs={'token':os.environ['HF_TOKEN']},
+        model_kwargs=dict(token=os.environ['HF_TOKEN'], device_map='auto'),
     )
 ]
 #        max_steps=3,
 #        max_out_len=1024,
+        # path='meta-llama/Llama-2-7b-hf',
+        # path="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
+        # model=base_model,
 
